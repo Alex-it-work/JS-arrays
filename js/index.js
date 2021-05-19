e56fd860febda8c678ad7318cc4223ae0ae4f621
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-05-19 11:51:42 +0300

    Finish tasks

--- js/index.js
+++ js/index.js
@@ -76,7 +76,7 @@ for (let i = 0; i < randomArray.length; i++) {
     counter++;
   }
 }
-console.log("counter :>> ", counter);
+console.log("counter 0:>> ", counter);
 
 // // Методы перебора массивов.
 // 7 Получить новый массив из заданного, который будет содержать только положительные числа (-1, 5, 0, 9, -10 => 5, 9).
@@ -118,10 +118,72 @@ positiveNegativeValuesArray.forEach(function (item) {
 
 showArray(powTwoArray);
 // 9 Проверить, являются ли все елементы массива положительными числами (* или в принципе числами).
-
 // Array.prototype.every()
+console.log(
+  "positiveNegativeValuesArray only positive? :>> ",
+  positiveNegativeValuesArray.every((currentValue) => currentValue > 0)
+);
 
+console.log(
+  "positiveValuesArray only positive? :>> ",
+  positiveValuesArray.every((currentValue) => currentValue > 0)
+);
+const nanArray = Array.from("Darling69");
+showArray(nanArray);
+console.log(
+  "nanArray has only numbers? :>> ",
+  nanArray.every((currentValue) => !isNaN(currentValue))
+);
+console.log(
+  "randomArray has only numbers? :>> ",
+  randomArray.every((currentValue) => !isNaN(currentValue))
+);
 // 10 Проверить, есть ли в массиве хоть один отрицательный элемент.
+
+console.log(
+  "positiveNegativeValuesArray has one or more negative value? :>> ",
+  positiveNegativeValuesArray.some((currentValue) => currentValue < 0)
+);
+
+console.log(
+  "positiveValuesArray has one or more negative value? :>> ",
+  positiveValuesArray.some((currentValue) => currentValue < 0)
+);
+
 // 11 Вывести элементы массива, возведенные в куб.
+randomArray.forEach((currentValue) =>
+  console.log("Math.pow(currentValue, 3) :>> ", Math.pow(currentValue, 3))
+);
 
 // *12 Прописать для MyArray метод unshift.
+
+function MyArray() {
+  if (!new.target) {
+    return new MyArray();
+  }
+  this.length = 0;
+}
+
+const myArrayProto = new MyArray();
+myArrayProto.unshift = function (item) {
+  for (let i = this.length; i >= 0; i--) {
+    i === 0 ? (this[0] = item) : (this[i] = this[i - 1]);
+  }
+  return ++this.length;
+};
+
+myArrayProto.push = function (item) {
+  this[this.length] = item;
+  return ++this.length;
+};
+
+MyArray.prototype = myArrayProto;
+
+const testArray = new MyArray();
+for (let i = 0; i < 5; i++) {
+  testArray.push(Math.floor(Math.random() * 5));
+}
+
+showArray(testArray);
+testArray.unshift(Math.floor(Math.random() * testArray.length));
+showArray(testArray);
